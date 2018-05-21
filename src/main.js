96b20acd9933c03bc804d1049a19f547d57a9bf8
ze25800000 <ze258100000@sina.com>
2018-05-21 19:33:32 +0800

    项目初始化页面-底部导航

--- src/main.js
+++ src/main.js
@@ -9,12 +9,35 @@ app.$mount()
 
 export default {
   config: {
-    pages: ['^pages/comments/main'],
+    pages: ['^pages/books/main'],
     window: {
       'backgroundTextStyle': 'light',
       'navigationBarBackgroundColor': '#EA5149',
       'navigationBarTitleText': '锡盟图书',
       'navigationBarTextStyle': 'light'
+    },
+    tabBar: {
+      selectedColor: '#EA5149',
+      list: [
+        {
+          pagePath: 'pages/books/main',
+          text: '图书',
+          iconPath: 'static/img/book.png',
+          selectedIconPath: 'static/img/book@selected.png'
+        },
+        {
+          pagePath: 'pages/comments/main',
+          text: '评论',
+          iconPath: 'static/img/comment.png',
+          selectedIconPath: 'static/img/comment@selected.png'
+        },
+        {
+          pagePath: 'pages/me/main',
+          text: '我的',
+          iconPath: 'static/img/me.png',
+          selectedIconPath: 'static/img/me@selected.png'
+        }
+      ]
     }
   }
 }
